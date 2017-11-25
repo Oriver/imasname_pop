9f774a726403dd259b6f69cbc02829e86f8893b9
Okugawar <prismrivers144@gmail.com>
2017-11-25 15:56:59 +0900

    first commit

---
+++ imasname_pop/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace imasname_pop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "POP!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(296, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(414, 34);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 131);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Im@s Pop";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ imasname_pop/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace imasname_pop
+{
+    public partial class Form1 : Form
+    {
+#if DEBUG
+        private const String CsvName = "imasname_pop.name_test.csv";
+#else
+        private const String CsvName = "imasname_pop.name.csv";
+#endif
+        private const String SaveFile = "data.dat";
+        private const String LogFile = "log.txt";
+
+        private Queue<int> queue = new Queue<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(SaveFile)) load();
+            else reset();
+        }
+
+        private void reset()
+        {
+            try
+            {
+                var lst = new List<int>();
+                var r = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(CsvName));
+                int count = 0;
+                while (r.ReadLine() != null) lst.Add(count++);
+
+                r.Close();
+
+                var rnd = new Random();
+
+                lst = lst.OrderBy((i) => rnd.Next()).ToList();
+                queue.Clear();
+
+                foreach (int i in lst) queue.Enqueue(i);
+
+                var w = new StreamWriter(new FileStream(LogFile, FileMode.Append));
+                w.WriteLine("=== RESET ===");
+                w.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private String pop()
+        {
+            if(queue.Count == 0)
+            {
+                reset();
+                return "出し切ったのでリセットします";
+            }
+            int no = queue.Dequeue();
+
+            try
+            {
+                var r = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream(CsvName), Encoding.GetEncoding("shift_jis"));
+                for (int i = no; i > 0; --i) r.ReadLine();
+                var str = r.ReadLine().Split(',');
+                var ret = str[0] + "/" + str[1];
+                r.Close();
+
+                var w = new StreamWriter(new FileStream(LogFile, FileMode.Append), Encoding.GetEncoding("shift_jis"));
+                w.WriteLine(ret);
+                w.Close();
+
+                return ret;
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return "";
+        }
+
+        private void save()
+        {
+            try
+            {
+                var w = new BinaryWriter(new FileStream(SaveFile, FileMode.Create));
+                w.Write(queue.Count);
+                while (queue.Count != 0) w.Write(queue.Dequeue());
+                w.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private void load()
+        {
+            try
+            {
+                var r = new BinaryReader(new FileStream(SaveFile, FileMode.Open));
+                int count = r.ReadInt32();
+                byte[] buf = new byte[count * 4];
+                r.Read(buf, 0, count * 4);
+                queue = new Queue<int>();
+                for (int i = 0; i < count; ++i) queue.Enqueue(BitConverter.ToInt32(buf, i * 4));
+                r.Close();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = pop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            save();
+        }
+    }
+}
